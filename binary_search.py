d518c8e1a6df6f5a9813c66d7871b461f31b5d2f
Talha Jamal <talhajamal11@gmail.com>
2023-02-17 17:01:21 +0500

    Amending Binary Search Function. Adding Comments and Print Statements for Debugging.

--- binary_search.py
+++ binary_search.py
@@ -1,24 +1,31 @@
 # Practicing a Binary Search Algorithm
 # Creating a Binary Search Algorithm that takes in a list and value to search for within the list
+import math
+
 
 def binary_search(arr, value):
     lower_bound = 0
-    upper_bound = len(arr)
+    upper_bound = len(arr) - 1
+    steps = 0
+    print(lower_bound, upper_bound)
 
-    mid = int((upper_bound) / 2)
-
-    count = 1
-
-    while arr[mid] != value:
-        if arr[mid] > value:
-            mid = int((lower_bound + mid) / 2)
-            count += 1
-        elif arr[mid] < value:
-            mid = int((upper_bound + mid) / 2)
-            count += 1
-    return arr[mid], count
+    while lower_bound <= upper_bound:
+        mid = math.floor(int((upper_bound + lower_bound) / 2))
+        print(mid)
+        if value == arr[mid]:
+            steps += 1
+            return mid, steps
+        elif value < arr[mid]:
+            upper_bound = mid - 1
+            steps += 1
+            print(upper_bound)
+        elif value > arr[mid]:
+            lower_bound = mid + 1
+            steps += 1
+            print(lower_bound)
+    return "Value not found in Array"
 
 
 if __name__ == '__main__':
-    array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    print(binary_search(array, 2))
+    array = [1, 2, 3, 4, 5, 6]
+    print(binary_search(array, 6))
